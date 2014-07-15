60a88ff7b169a5b0ee8e29f4efa13ce651e2412a
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2014-07-15 23:22:24 +0530

    the almost perfect ui prototyping env.

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    // Watch for changes and trigger compass and livereload
+    watch: {
+      compass: {
+        files: ['sass/{,**/}*.scss'],
+        tasks: ['compass:dev']
+      },
+      livereload: {
+        options: {
+          livereload: true
+        },
+        files: [
+	  '*.html',
+          'css/style.css',
+          'js/*.js',
+          'images/{,**/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // Compass and scss
+    compass: {
+      options: {
+        cssDir: 'css',
+        sassDir: 'sass',
+        imagesDir: 'images',
+        javascriptsDir: 'js',
+        fontsDir: 'css/fonts',
+        assetCacheBuster: 'none',
+        require: [
+          'sass-globbing',
+          'susy',
+          'breakpoint'
+        ]
+      },
+	dev: {
+        options: {
+          environment: 'development',
+          outputStyle: 'expanded',
+          relativeAssets: true,
+          raw: 'line_numbers = :true\n'
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production',
+          outputStyle: 'compact',
+          force: true
+        }
+      }
+    }
+
+});
+
+  // Load the plugins.
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask('default', [
+    'compass:dev',
+    'watch'
+  ]);
+
+};
